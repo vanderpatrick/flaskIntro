e27d8071e4633c32ab7ea5e85dc7ae7e454c251c
vanderpatrick <patrick123321123@icloud.com>
2022-07-24 10:58:36 +0000

    Import request from flask to get form data from contact.html

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 
@@ -25,11 +25,13 @@ def about_member(member_name):
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return "<h1>" + member["description"] + "</h1>"           
+    return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print("hello")
     return render_template("contact.html", page_title="Contact")
 
 
